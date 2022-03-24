15c0f7f8c38eaa4cb319e1a90fd8768b60a93156
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-03-24 03:57:27 -0300

    feat(express): add initial app setup

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,16 +1,20 @@
 import * as express from 'express';
+import bodyParser = require('body-parser');
+import loginRoute from './api/routes/login.route';
+import errorHandler from './api/middlewares/errorhandler.middleware';
 
 class App {
   public app: express.Express;
-  // ...
 
   constructor() {
-    // ...
+    this.app = express();
+    this.middlewares();
+    this.routes();
+    this.errors();
     this.config();
-    // ...
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
@@ -19,12 +23,22 @@ class App {
     };
 
     this.app.use(accessControl);
-    // ...
   }
 
-  // ...
-  public start(PORT: string | number):void {
-    // ...
+  public middlewares(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  public routes(): void {
+    this.app.use('/login', loginRoute);
+  }
+
+  public errors(): void {
+    this.app.use(errorHandler);
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`listening on port ${PORT}`));
   }
 }
 
