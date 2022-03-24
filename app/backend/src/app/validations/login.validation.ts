63136d6fbb82ed9a3321b8af193a988d3de23956
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-03-24 03:54:45 -0300

    feat(app): create login validation

--- app/backend/src/app/validations/login.validation.ts
+++ app/backend/src/app/validations/login.validation.ts
@@ -1,16 +1,12 @@
 import * as Joi from 'joi';
+import { UserCredentials } from '../domain';
 import {
   emailIsRequired,
   passwordIsRequired,
   loginIsInvalid,
 } from './errors/login.error';
 
-interface ILogin {
-  email: string;
-  password: string;
-}
-
-class Login {
+class LoginValidation {
   private _isRequired = Joi.object({
     email: Joi.string().required().error(emailIsRequired),
     password: Joi.string().required().error(passwordIsRequired),
@@ -21,10 +17,10 @@ class Login {
     password: Joi.string().min(6),
   }).error(loginIsInvalid);
 
-  async validate(body: ILogin) {
+  public async validate(body: UserCredentials) {
     await this._isRequired.validateAsync(body);
     return this._isValid.validateAsync(body);
   }
 }
 
-export default new Login();
+export default new LoginValidation();
