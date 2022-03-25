38306d9af0795990740d20def4b676350769b959
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-03-25 01:14:05 -0300

    feat(app): implement login function completely

--- app/backend/src/app/services/login.service.ts
+++ app/backend/src/app/services/login.service.ts
@@ -1,6 +1,10 @@
 import * as jwt from 'jsonwebtoken';
 import * as fs from 'fs/promises';
+import * as bcrypt from 'bcrypt';
 import { UserCredentials } from '../domain';
+
+import User from '../../database/models/User.model';
+import INCORRECT_EMAIL_OR_PASSWORD from './errors/login.error';
 
 class LoginService {
   private _secret: string;
@@ -15,8 +19,28 @@ class LoginService {
     });
   }
 
-  public login(login: UserCredentials) {
-    return jwt.sign(login, this._secret);
+  private _mapUser(param: User) {
+    const user = {
+      id: param.id,
+      username: param.username,
+      role: param.role,
+      email: param.email,
+    };
+
+    return {
+      user,
+      token: jwt.sign(user, this._secret),
+    };
+  }
+
+  public async login(login: UserCredentials) {
+    const user = await User.findOne({
+      where: { email: login.email },
+    });
+    if (user === null) throw INCORRECT_EMAIL_OR_PASSWORD;
+    const passwordIsValid = await bcrypt.compare(login.password, user.password);
+    if (passwordIsValid) return this._mapUser(user);
+    throw INCORRECT_EMAIL_OR_PASSWORD;
   }
 }
 
