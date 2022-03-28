f828fb1d81ae57c8da72a5e0a44688217117e65f
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-03-27 22:38:11 -0300

    refactor(tests): simplify the code

---
+++ app/backend/src/tests/utils/login.util.ts
@@ -0,0 +1,17 @@
+export const userResolve = {
+  id: 1,
+  username: 'Username',
+  role: 'role',
+  email: 'email@email.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+};
+
+export const body_ok = {
+  email: 'email@email.com',
+  password: 'secret_password',
+};
+
+export const body_wrong = {
+  email: 'wrong_email',
+  password: 'wrong_password',
+};
