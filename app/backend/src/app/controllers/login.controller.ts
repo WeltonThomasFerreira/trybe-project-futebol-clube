7308a33ef3d050877e999cc617879b53847f84fd
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-03-28 01:34:55 -0300

    refactor(app): add return type loginService.login

--- app/backend/src/app/controllers/login.controller.ts
+++ app/backend/src/app/controllers/login.controller.ts
@@ -1,13 +1,13 @@
 import loginValidation from '../validations/login.validation';
 import loginService from '../services/login.service';
-import { UserCredentials } from '../domain';
+import { UserCredentials, UserLogged } from '../domain';
 
 export class LoginController {
   private _loginValidation = loginValidation;
 
   private _loginService = loginService;
 
-  public async post(body: UserCredentials) {
+  public async post(body: UserCredentials): Promise<UserLogged> {
     const userCredentials = await this._loginValidation.validate(body);
     return this._loginService.login(userCredentials);
   }
--- app/backend/src/app/domain.ts
+++ app/backend/src/app/domain.ts
@@ -2,3 +2,13 @@ export interface UserCredentials {
   email: string;
   password: string;
 }
+
+export interface UserLogged {
+  user: {
+    id: number;
+    username: string;
+    role: string;
+    email: string;
+  };
+  token: string;
+}
